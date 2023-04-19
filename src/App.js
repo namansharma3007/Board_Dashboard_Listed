06e61f6d50245e34f01e6f72fa4f727c331fbb7a
Naman Sharma <96349715+namansharma3007@users.noreply.github.com>
2023-04-19 18:37:17 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -12,8 +12,7 @@ function App() {
       const url = "https://google-auth-board-listed.up.railway.app/auth/login/success";
       // const url = "http://localhost:8080/auth/login/success";
       
-      // const response = await axios.get(url, { withCredentials: true });
-      const response = await axios.get(url);
+      const response = await axios.get(url, { withCredentials: true });
       setUser(response.data.user._json)
     } catch (err) {
       console.error(err);
